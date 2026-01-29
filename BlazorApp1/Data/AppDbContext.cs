ad01f8610d3ec4a1a4bf0605b095dc719967003c
devuser <devuser@local>
2026-01-29 15:23:17 +0000

    Made progress on DB connection, setup a db on a remote guest

---
+++ BlazorApp1/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorApp1.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
